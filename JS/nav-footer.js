a65e322f197c8d25412a68338ab6ef13b0e27ae6
Leen Aboukhalil <109052421+Leen-ak@users.noreply.github.com>
2025-03-28 22:17:24 -0400

    Change the path of the files, still the website not working

--- JS/nav-footer.js
+++ JS/nav-footer.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("navbar.html")
+    fetch("HTML/navbar.html")
         .then(res => res.text())
         .then(data => {
             document.getElementById("navbar-container").innerHTML = data;
         });
 
-    fetch("footer.html")
+    fetch("HTML/footer.html")
         .then(res => res.text())
         .then(data => {
             document.getElementById("footer-container").innerHTML = data;
